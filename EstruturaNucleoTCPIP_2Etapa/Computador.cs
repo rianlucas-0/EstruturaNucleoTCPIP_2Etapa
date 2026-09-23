d2c03d9a38b4c97083c9a655aa3c1f21ddb2af86
Rian Lucas <rianlucas.moon@gmail.com>
2026-09-23 19:20:54 -0300

    feat: adiciona relação entre computador e camada de aplicação

--- EstruturaNucleoTCPIP_2Etapa/Computador.cs
+++ EstruturaNucleoTCPIP_2Etapa/Computador.cs
@@ -7,14 +7,16 @@ public class Computador
     public string Nome = "";
     public string Ip = "";
     public string Mac = "";
+    public PduAplicacao Dados;
     public void Ligar()
     {
         Console.WriteLine($"Ligando o Computador: {Nome}");
     }
-    public Computador(string nome, string ip, string mac)
+    public Computador(string nome, string ip, string mac, PduAplicacao pduA)
     {
         Nome = nome;
         Ip = ip;
         Mac = mac;
+        Dados = pduA;
     }
 }
--- EstruturaNucleoTCPIP_2Etapa/Program.cs
+++ EstruturaNucleoTCPIP_2Etapa/Program.cs
@@ -3,24 +3,19 @@ Console.WriteLine("             NÚCLEO TCP/IP EM EXECUÇÃO");
 Console.WriteLine("                  GRUPO 1 | 2026/2");
 Console.WriteLine("==================================================");
 
-Computador pc1 = new Computador("PC-01", "192.168.0.1", "02:4A:7F:B3:91:C8");
-
-Computador pc2 = new Computador("PC-02", "192.168.0.2", "02:D6:3E:85:AC:17");
-
-PduAplicacao aplicacaoA = new PduAplicacao("Mensagem de teste");
-
+PduAplicacao aplicacaoA = new PduAplicacao("Mensagem de teste!!");
 PduAplicacao aplicacaoB = new PduAplicacao("");
 
-Transporte transporteA = new Transporte();
+Computador pc1 = new Computador("PC-01", "192.168.0.1", "02:4A:7F:B3:91:C8", aplicacaoA);
+Computador pc2 = new Computador("PC-02", "192.168.0.2", "02:D6:3E:85:AC:17", aplicacaoB);
 
+Transporte transporteA = new Transporte();
 Transporte transporteB = new Transporte();
 
 Rede redeA = new Rede();
-
 Rede redeB = new Rede();
 
 Interface interfaceA = new Interface();
-
 Interface interfaceB = new Interface();
 
 pc1.Ligar();
@@ -32,15 +27,12 @@ Console.WriteLine("\n------------------- ENVIO -------------------");
 aplicacaoA.EnviarDados();
 
 var pduTransporte = transporteA.Pack("10", "10", aplicacaoA);
-
 transporteA.EnviarDados();
 
 var pduRede = redeA.Pack(pc1.Ip, pc2.Ip, pduTransporte);
-
 redeA.EnviarDados();
 
 var pduInterface = interfaceA.Pack(pc1.Mac, pc2.Mac, pduRede);
-
 interfaceA.EnviarDados();
 
 Console.WriteLine("\n---------------- RECEBIMENTO ----------------");
@@ -48,13 +40,10 @@ Console.WriteLine("\n---------------- RECEBIMENTO ----------------");
 var pduInterfaceR = interfaceB.UnPack(pduInterface);
 
 interfaceB.ReceberDados();
-
 var pduRedeR = redeB.UnPack(pduInterfaceR);
 
 redeB.ReceberDados();
-
 var pduTransporteR = transporteB.UnPack(pduRedeR);
-
 transporteB.ReceberDados();
 
 aplicacaoB.Dados = pduTransporteR.Dados;
